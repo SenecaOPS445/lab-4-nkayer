059dcacd4690207ed8dc299275514c1410b0bafa
Narendra Ayer <nkayer@myseneca.ca>
2025-02-10 09:08:24 -0500

    Added lab4a.py

--- lab-4-nkayer/lab4a.py
+++ lab-4-nkayer/lab4a.py
@@ -2,15 +2,15 @@
 
 def join_sets(s1, s2):
     
-    return s1 | s2  
+    return s1.union(s2)
 
 def match_sets(s1, s2):
     
-    return s1 & s2  
+    return s1.intersection(s2)
 
 def diff_sets(s1, s2):
     
-    return s1 ^ s2  
+    return s1.symmetric_difference(s2)
 
 if __name__ == '__main__':
     set1 = set(range(1, 10))
